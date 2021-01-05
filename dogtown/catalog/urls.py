a8a7b102ff36e59cdf2fe64b3709611f39e8e695
igorg9810 <turgon98@gmail.com>
2021-01-05 17:48:29 +0300

    add: new dogs

--- dogtown/catalog/urls.py
+++ dogtown/catalog/urls.py
@@ -6,4 +6,11 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('akita', views.akita, name='akita'),
     path('store', views.store, name='store'),
+    path('kelpi', views.kelpi, name='kelpi'),
+    path('beagle', views.beagle, name='beagle'),
+    path('bobtail', views.bobtail, name='bobtail'),
+    path('pitbull', views.pitbull, name='pitbull'),
+    path('bulldog', views.bulldog, name='bulldog'),
+    path('dalmatian', views.dalmatian, name='dalmatian'),
+    path('cocker', views.cocker, name='cocker'),
 ]
